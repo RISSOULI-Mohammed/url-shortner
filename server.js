053dfb537fc4001d1031cd05ef674640adad99f5
Glitch (best-donkey) <none>
2018-02-08 08:52:54 +0000

    🐮🐦 Checkpoint server.js:667310/3

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get("/", function (request, response) {
 });
 
 app.get("/new/:url", function(request, response){
+  var url = request.params["url"];
   
   //response.end(client.urlshortner(request.params["url"]));
 });
