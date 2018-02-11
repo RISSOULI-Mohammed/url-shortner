8cbb4999fbf25a87a1cc2f2ebdf64ba709d8bb02
Glitch (best-donkey) <none>
2018-02-11 15:06:07 +0000

    📉🐽 Checkpoint server.js:667310/118

--- server.js
+++ server.js
@@ -16,48 +16,56 @@ var MongoClient = mongodb.MongoClient;
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/new/*", function(request, response){
-  var url = request.originalUrl;
-  url = url.replace("/new/", "");
-  var id = 0;
-  MongoClient.connect("mongodb://omisimo:omisimo@ds229418.mlab.com:29418/shortner", function (err, dbo) {
-  if (err) {
-    throw err;
-  } else {
-    var db = dbo.db("shortner");
-    var coll1 = db.collection("counters")
-    var sequence = coll1.findAndModify(
-      {_id: "urlid" },
-      [['_id','asc']],
-      {$inc:{sequence_value:1}},
-      {},
-      function(err, object){
-        id = object.value.sequence_value;
-        var doc = {"_id": id, "url": url};
-    //var doc = {"url": url};
-    var coll = db.collection("url");
-    coll.insert(doc, function(err, data){
-      if (err) {
-    throw err;
-  } else {
-    response.end(JSON.stringify(doc));
-  }
+app.get("/new/*", function(request, response) {
+    var url = request.originalUrl;
+    url = url.replace("/new/", "");
+
+
+    var id = 0;
+    MongoClient.connect("mongodb://omisimo:omisimo@ds229418.mlab.com:29418/shortner", function(err, dbo) {
+        if (err) {
+            throw err;
+        }
+        else {
+            var db = dbo.db("shortner");
+            var coll1 = db.collection("counters")
+            var sequence = coll1.findAndModify({ _id: "urlid" }, [
+                    ['_id', 'asc']
+                ], { $inc: { sequence_value: 1 } }, {},
+                function(err, object) {
+                    id = object.value.sequence_value;
+                    var doc = { "_id": id, "url": url };
+                    //var doc = {"url": url};
+                    var coll = db.collection("url");
+                    coll.insert(doc, function(err, data) {
+                        if (err) {
+                            throw err;
+                        }
+                        else {
+                            var output = {
+                                "original_url": url,
+                                "short_url": "https://best-donkey.glitch.me/" + id
+                            };
+                            response.end(JSON.stringify(output));
+                        }
+                    });
+                }
+            );
+
+
+
+        }
     });
-      }
-   );
-	
-    
-    dbo.close();
-  }
-});
-  //response.end(client.urlshortner(request.params["url"]));
+
+
+    //response.end(client.urlshortner(request.params["url"]));
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
