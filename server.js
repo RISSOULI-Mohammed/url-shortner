38b4f4257f12a5d2ef307083ee044e867b931917
Glitch (best-donkey) <none>
2018-02-11 14:01:24 +0000

    🏈🎥 Checkpoint server.js:667310/241

--- server.js
+++ server.js
@@ -4,6 +4,9 @@
 // init project
 var express = require('express');
 var app = express();
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -18,7 +21,23 @@ app.get("/", function (request, response) {
 
 app.get("/new/:url", function(request, response){
   var url = request.params["url"];
-  
+  var doc = {};
+  MongoClient.connect("mongodb://omisimo:omisimo@ds229418.mlab.com:29418/", function (err, dbo) {
+  if (err) {
+    throw err;
+  } else {
+    var db = dbo.db("shortner");
+    var coll = db.collection("url");
+    coll.insert(doc, function(err, data){
+      if (err) {
+    throw err;
+  } else {
+    response.end(url);
+  }
+    });
+    dbo.close();
+  }
+});
   //response.end(client.urlshortner(request.params["url"]));
 });
 
